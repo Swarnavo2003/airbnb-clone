037baeb2c0b4ec50485015a48d8a9626c5c5872d
Swarnavo2003 <swarnavo2020@gmail.com>
2025-11-12 17:08:10 +0530

    feat:create room controller added

---
+++ src/main/java/in/swarnavo/airbnb/controller/RoomAdminController.java
@@ -0,0 +1,24 @@
+package in.swarnavo.airbnb.controller;
+
+import in.swarnavo.airbnb.dto.RoomDto;
+import in.swarnavo.airbnb.service.RoomService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/admin/hotels/{hotelId}/rooms")
+@RequiredArgsConstructor
+public class RoomAdminController {
+
+    private final RoomService roomService;
+
+    @PostMapping
+    public ResponseEntity<RoomDto> createNewRoom(@PathVariable Long hotelId, @RequestBody RoomDto roomDto) {
+        RoomDto room = roomService.createNewRoom(hotelId, roomDto);
+        return new ResponseEntity<>(room, HttpStatus.CREATED);
+    }
+
+
+}
