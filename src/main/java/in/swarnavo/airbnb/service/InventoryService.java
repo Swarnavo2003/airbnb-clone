e99c59505e493f4eacc50382b4a3bfc03711169a
Swarnavo2003 <swarnavo2020@gmail.com>
2025-11-14 00:10:06 +0530

    feat: search hotels with start and end date availability constraints added

--- src/main/java/in/swarnavo/airbnb/service/InventoryService.java
+++ src/main/java/in/swarnavo/airbnb/service/InventoryService.java
@@ -1,9 +1,14 @@
 package in.swarnavo.airbnb.service;
 
+import in.swarnavo.airbnb.dto.HotelDto;
+import in.swarnavo.airbnb.dto.HotelSearchRequest;
 import in.swarnavo.airbnb.entity.Room;
+import org.springframework.data.domain.Page;
 
 public interface InventoryService {
     void initializeRoomForAYear(Room room);
 
     void deleteAllInventories(Room room);
+
+    Page<HotelDto> searchHotels(HotelSearchRequest hotelSearchRequest);
 }
--- src/main/java/in/swarnavo/airbnb/service/InventoryServiceImpl.java
+++ src/main/java/in/swarnavo/airbnb/service/InventoryServiceImpl.java
@@ -1,14 +1,22 @@
 package in.swarnavo.airbnb.service;
 
+import in.swarnavo.airbnb.dto.HotelDto;
+import in.swarnavo.airbnb.dto.HotelSearchRequest;
+import in.swarnavo.airbnb.entity.Hotel;
 import in.swarnavo.airbnb.entity.Inventory;
 import in.swarnavo.airbnb.entity.Room;
 import in.swarnavo.airbnb.repository.InventoryRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.modelmapper.ModelMapper;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
 
 @Service
 @RequiredArgsConstructor
@@ -16,6 +24,7 @@ import java.time.LocalDate;
 public class InventoryServiceImpl implements InventoryService {
 
     private final InventoryRepository inventoryRepository;
+    private final ModelMapper modelMapper;
 
     @Override
     public void initializeRoomForAYear(Room room) {
@@ -40,5 +49,22 @@ public class InventoryServiceImpl implements InventoryService {
     @Override
     public void deleteAllInventories(Room room) {
         inventoryRepository.deleteByRoom(room);
+    }
+
+    @Override
+    public Page<HotelDto> searchHotels(HotelSearchRequest hotelSearchRequest) {
+        Pageable pageable = PageRequest.of(hotelSearchRequest.getPage(), hotelSearchRequest.getSize());
+        long dateCount = ChronoUnit.DAYS.between(hotelSearchRequest.getStartDate(), hotelSearchRequest.getEndDate()) + 1;
+
+        Page<Hotel> hotelPage = inventoryRepository.findHotelsWithAvailableInventory(
+                hotelSearchRequest.getCity(),
+                hotelSearchRequest.getStartDate(),
+                hotelSearchRequest.getEndDate(),
+                hotelSearchRequest.getRoomsCount(),
+                dateCount,
+                pageable
+        );
+
+        return hotelPage.map((hotel) -> modelMapper.map(hotel, HotelDto.class));
     }
 }
