fcb21837cf9fabdbd1654b53e7bf44392eda16b1
Swarnavo2003 <swarnavo2020@gmail.com>
2025-11-15 11:17:53 +0530

    feat: pricing strategy added and all strategies implemented in decorator pattern

---
+++ src/main/java/in/swarnavo/airbnb/strategy/PricingService.java
@@ -0,0 +1,21 @@
+package in.swarnavo.airbnb.strategy;
+
+import in.swarnavo.airbnb.entity.Inventory;
+import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
+
+@Service
+public class PricingService {
+    public BigDecimal calculateDynamicPricing(Inventory inventory) {
+        PriceStrategy pricingStrategy = new BasePricingStrategy();
+
+        // apply the additional strategy
+        pricingStrategy = new SurgePricingStrategy(pricingStrategy);
+        pricingStrategy = new OccupancyPricingStrategy(pricingStrategy);
+        pricingStrategy = new UrgencyPricingStrategy(pricingStrategy);
+        pricingStrategy = new HolidayPricingStrategy(pricingStrategy);
+
+        return pricingStrategy.calculatePrice(inventory);
+    }
+}
