a18f11fb23069fa275d7dcca8ed505f62afea7ec
Swarnavo2003 <swarnavo2020@gmail.com>
2025-11-15 11:04:01 +0530

    feat: holiday pricing strategy added

---
+++ src/main/java/in/swarnavo/airbnb/strategy/HolidayPricingStrategy.java
@@ -0,0 +1,24 @@
+package in.swarnavo.airbnb.strategy;
+
+import in.swarnavo.airbnb.entity.Inventory;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
+
+@Service
+@RequiredArgsConstructor
+public class HolidayPricingStrategy implements PriceStrategy {
+
+    private final PriceStrategy wrapped;
+
+    @Override
+    public BigDecimal calculatePrice(Inventory inventory) {
+        BigDecimal price = wrapped.calculatePrice(inventory);
+        boolean isTodayHoliday = true; // call an API or check in an array containing the holidays date
+        if(isTodayHoliday) {
+            price = price.multiply(BigDecimal.valueOf(1.25));
+        }
+        return price;
+    }
+}
