d63ec0bc718e3d6d3dc21da192d44dde4b7dd485
Swarnavo2003 <swarnavo2020@gmail.com>
2025-11-15 10:47:48 +0530

    feat: occupany pricing strategy added

--- src/main/java/in/swarnavo/airbnb/strategy/OccupancyPricingStrategy.java
+++ src/main/java/in/swarnavo/airbnb/strategy/OccupancyPricingStrategy.java
@@ -1,4 +1,24 @@
 package in.swarnavo.airbnb.strategy;
 
-public class OccupancyPricingStrategy {
+import in.swarnavo.airbnb.entity.Inventory;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
+
+@Service
+@RequiredArgsConstructor
+public class OccupancyPricingStrategy implements PriceStrategy {
+
+    private final PriceStrategy wrapped;
+
+    @Override
+    public BigDecimal calculatePrice(Inventory inventory) {
+        BigDecimal price = wrapped.calculatePrice(inventory);
+        double occupancyRate = (double) inventory.getBookedCount() / inventory.getTotalCount();
+        if(occupancyRate > 0.8) {
+            price = price.multiply(BigDecimal.valueOf(2));
+        }
+        return price;
+    }
 }
