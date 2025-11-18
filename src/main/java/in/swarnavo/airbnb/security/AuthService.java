e5392125eeb42aa1271b6de744a88d5b7705dc3f
Swarnavo2003 <swarnavo2020@gmail.com>
2025-11-18 11:36:45 +0530

    feat: signup service added

--- src/main/java/in/swarnavo/airbnb/security/AuthService.java
+++ src/main/java/in/swarnavo/airbnb/security/AuthService.java
@@ -1,12 +1,39 @@
 package in.swarnavo.airbnb.security;
 
+import in.swarnavo.airbnb.dto.SignUpRequestDto;
+import in.swarnavo.airbnb.dto.UserDto;
+import in.swarnavo.airbnb.entity.User;
+import in.swarnavo.airbnb.entity.enums.Role;
+import in.swarnavo.airbnb.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.util.Set;
 
 @Service
 @RequiredArgsConstructor
 public class AuthService {
 
+    private final UserRepository userRepository;
+    private final ModelMapper modelMapper;
+    private final PasswordEncoder passwordEncoder;
 
+    public UserDto signUp(SignUpRequestDto signUpRequestDto) {
+        User user = userRepository
+                .findByEmail(signUpRequestDto.getEmail())
+                .orElse(null);
 
+        if(user != null) {
+            throw new RuntimeException("User already present with same email id");
+        }
+
+        User newUser = modelMapper.map(signUpRequestDto, User.class);
+        newUser.setRoles(Set.of(Role.GUEST));
+        newUser.setPassword(passwordEncoder.encode(signUpRequestDto.getPassword()));
+        newUser = userRepository.save(newUser);
+
+        return modelMapper.map(newUser, UserDto.class);
+    }
 }
